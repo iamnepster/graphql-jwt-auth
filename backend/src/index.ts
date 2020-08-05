06219664147bb2e15cd2c47183db012e111c889e
Jan Völker <jan-voelker@t-online.de>
2020-08-05 19:54:48 +0200

    feat(errorhandling): added errorhandling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import 'reflect-metadata'
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import { ApolloServer } from 'apollo-server-express'
 import { buildSchema } from 'type-graphql'
 import { AuthResolvers } from './api/auth-resolvers'
+import cors from 'cors'
 
 const app = express()
+app.use(cors({ origin: 'http://localhost:3000' }))
 
 const port = process.env.PORT || 8080
 
@@ -22,6 +24,21 @@ const boostrap = async () => {
   })
 
   apolloServer.applyMiddleware({ app })
+
+  app.use((req, res, next) => {
+    const error = new Error(`Not found - ${req.originalUrl}`)
+    res.status(404)
+    next(error)
+  })
+
+  app.use((error: Error, _req: Request, res: Response, _next: NextFunction) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    res.status(statusCode)
+    res.json({
+      message: error.message,
+      stack: process.env.NODE_ENV === 'production' ? '' : error.stack
+    })
+  })
 
   app.listen(port, () => {
     console.log(`Listening on port http://localhost:${port}`)
